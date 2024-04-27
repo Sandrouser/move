64e103a2f42e32f19ec29f4e892ca33eeed206a6
Sandrouser <dzamashvilisandro@gmail.com>
2024-04-27 18:23:10 +0400

    Initial commit

---
+++ js.js
@@ -0,0 +1,44 @@
+// const div = document.querySelector(".maindiv");
+
+// // function func(event) {
+// //   div.style.top = event.clientY + "px";
+// //   div.style.left = event.clientX + "px";
+// //   console.log(event);
+// // }
+
+// // let x = 0;
+
+// // document.addEventListener("keydown", (e) => {
+// //   console.log(e);
+
+// //   if (e.key === "ArrowRight") {
+// //     x += 20;
+// //     div.style.left = x + "px";
+// //   }
+// //   if (e.key === "ArrowLeft") {
+// //     x -= 20;
+// //     div.style.left = x + "px";
+// //   }
+// // });
+
+// // div.addEventListener("mousedown", (e) => {
+// //   document.addEventListener("mousemove", func);
+// // });
+// // document.addEventListener("mouseup", (e) => {
+// //   document.removeEventListener("mousemove", func);
+// // });
+
+// let y = 0;
+// while (y < 100) {
+//    y = y + 20;
+//   console.log(y);
+// }
+
+// for(let i=0; i<3;i+=1){
+//     console.log(i);
+// }
+
+let user={
+    name:"sandro",
+    age:14
+}
